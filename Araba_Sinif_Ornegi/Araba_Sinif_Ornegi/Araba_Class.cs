ebd17236567fa3a56146d956146e84e38f5a6c08
Samet Talha Tozlu <111143783+samettalhatozlu@users.noreply.github.com>
2022-08-13 14:33:25 +0300

    C# Class Example
    Vehicle Information Card Example with C#

---
+++ Araba_Sinif_Ornegi/Araba_Sinif_Ornegi/Araba_Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Araba_Sinif_Ornegi
+{
+    internal class Araba_Class
+    {
+        public string renk;
+        public string marka;
+        public int model;
+        public double motor;
+    }
+}
---
+++ Araba_Sinif_Ornegi/Araba_Sinif_Ornegi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Araba_Sinif_Ornegi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba_Class araba = new Araba_Class();
+            araba.marka = "BMW";
+            araba.renk = "Siyah";
+            araba.model = 2022;
+            araba.motor = 1.6;
+
+            Console.WriteLine("$$$ ARABA TANITIM KARTI $$$");
+            Console.WriteLine();
+            Console.WriteLine("Arabanın markası: " + araba.marka);
+            Console.WriteLine("Arabanın modeli: " + araba.model);
+            Console.WriteLine("Arabanın rengi: " + araba.renk);
+            Console.WriteLine("Arabanın motor gücü: " + araba.motor);
+
+            Console.Read();
+        }
+    }
+}
